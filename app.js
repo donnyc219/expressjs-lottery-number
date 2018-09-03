3f3c40ddce575f787f52be871d7422e9e1de9b56
Donny Cheng <donccl@gmail.com>
2018-09-03 12:40:10 -0400

    added cors library added 2 temporary apis

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// powerball
+// https://data.ny.gov/resource/8vkr-v8vh.json
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// megamillio
+// https://data.ny.gov/resource/h6w8-42p9.json
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/getLatestPowerball', (req, res) => {
+    axios.get("https://data.ny.gov/resource/8vkr-v8vh.json")
+    .then(response => {
+        res.send(response.data[0])
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+app.get('/getLatestMegaMillion', (req, res) => {
+    axios.get("https://data.ny.gov/resource/h6w8-42p9.json")
+    .then(response => {
+        res.send(response.data[0])
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
