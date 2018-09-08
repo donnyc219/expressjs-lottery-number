9678b011839b2e353497e70278d7b5372d4fc34a
Donny Cheng <donccl@gmail.com>
2018-09-08 11:18:34 -0400

    change the type of output properties so that angular app does not have to change anything

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/getLatestPowerball', (req, res) => {
     axios.get("https://data.ny.gov/resource/8vkr-v8vh.json")
     .then(response => {
-        res.send(response.data[0])
+        // res.send(response.data[0])
+        const firstPowerball = response.data[0];
+        var numsString = firstPowerball["winning_numbers"]
+        var output = {
+            draw_date: firstPowerball["draw_date"],
+            multiplier: firstPowerball["multiplier"],
+            winning_numbers: numsString.split(" ")
+        };
+
+        res.send(output)
     })
     .catch(error => {
         console.log(error)
@@ -28,11 +37,19 @@ app.get('/getLatestPowerball', (req, res) => {
 app.get('/getLatestMegaMillion', (req, res) => {
     axios.get("https://data.ny.gov/resource/h6w8-42p9.json")
     .then(response => {
-        res.send(response.data[0])
+        const firstMegaMillion = response.data[0];
+        var numsString = firstMegaMillion["winning_numbers"]
+        var output = {
+            draw_date: firstMegaMillion["draw_date"],
+            mega_ball: firstMegaMillion["mega_ball"],
+            multiplier: firstMegaMillion["multiplier"],
+            winning_numbers: numsString.split(" ")
+        }
+        res.send(output)
     })
     .catch(error => {
         console.log(error)
     })
 })
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.listen(8000, () => console.log('Example app listening on port 8080!'))
